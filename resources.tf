42f9b3ba975d402aa8e9a773914d006054e8ae5a
Patrick Ortell <p5150j@gmail.com>
2019-08-08 10:59:13 -0600

    adding subnets

--- resources.tf
+++ resources.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "env-example" {
     Name = "pj-aws-vpc-boilerplate"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+  cidr_block        = "${cidrsubnet(aws_vpc.env-example.cidr_block, 3, 1)}"
+  vpc_id            = "${aws_vpc.env-example.id}"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block        = "${cidrsubnet(aws_vpc.env-example.cidr_block, 2, 2)}"
+  vpc_id            = "${aws_vpc.env-example.id}"
+  availability_zone = "us-west-2b"
+}
